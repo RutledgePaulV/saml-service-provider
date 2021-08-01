975addd5ba1d00beef37280dd7c343c86ea547e8
Paul Rutledge <rutledgepaulv@gmail.com>
2021-07-31 21:19:08 -0500

    Version 0.1.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.rutledgepaulv/saml-service-provider "0.1.4"
+(defproject org.clojars.rutledgepaulv/saml-service-provider "0.1.5-SNAPSHOT"
 
   :description
   "Middleware for implementing a saml service provider."
